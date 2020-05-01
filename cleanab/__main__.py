309c725b28024d8b648eaa717a147c201d0bb7d7
Jan Willhaus <mail@janwillhaus.de>
2020-05-01 23:06:48 +0200

    Properly handle root logger through logzero as well

--- cleanab/__main__.py
+++ cleanab/__main__.py
@@ -1,3 +1,10 @@
-from cleanab.main import main
+import logging
+
+import logzero
+
+logzero.__name__ = ""
+logzero.setup_logger(level=logging.WARNING)
+
+from cleanab.main import main  # noqa: F401
 
 main()
