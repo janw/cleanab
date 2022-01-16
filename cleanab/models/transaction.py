7f9ef4d08ae926064d78bbda039cbf61f00f8044
Jan Willhaus <mail@janwillhaus.de>
2022-01-16 13:00:44 +0100

    Coalesce `None` purpose to empty string

--- cleanab/models/transaction.py
+++ cleanab/models/transaction.py
@@ -1,11 +1,15 @@
 from datetime import date
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class FintsTransaction(BaseModel):
     date: date
     amount: int
     applicant_name: str
-    purpose: str
+    purpose: str = ""
     import_id: str = ""
+
+    @validator("purpose", pre=True)
+    def set_purpose_empty(cls, purpose):
+        return purpose or ""
